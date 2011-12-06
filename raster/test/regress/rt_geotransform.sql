6d4383506c810468db4083f70c74bce14db26202
Bryce Nordgren <bnordgren@gmail.com>
2011-12-06 23:06:13 +0000

    Added regression tests for geotransform accessors.

---
+++ raster/test/regress/rt_geotransform.sql
@@ -0,0 +1,27 @@
+SELECT (gt).imag as imag, 
+       (gt).jmag as jmag, 
+       degrees((gt).theta_i) as theta_i, 
+       degrees((gt).theta_ij) as theta_ij
+   FROM (SELECT ST_GetGeotransform(ST_MakeEmptyRaster(10,10, 0,0, 20)) as gt) as dummy  ; 
+
+SELECT (gt).imag as imag, 
+       (gt).jmag as jmag, 
+       degrees((gt).theta_i) as theta_i, 
+       degrees((gt).theta_ij) as theta_ij
+   FROM 
+      (SELECT 
+          ST_GetGeotransform(
+             ST_SetScale(
+               ST_SetSkew(
+                 ST_MakeEmptyRaster(10,10,0,0,20), 
+                 -sqrt(2), sqrt(2)), 
+               sqrt(2))) as gt) as dummy ;
+
+
+SELECT (gt).imag as imag, 
+       (gt).jmag as jmag, 
+       degrees((gt).theta_i) as theta_i, 
+       degrees((gt).theta_ij) as theta_ij
+  FROM (SELECT ST_GetGeotransform(
+               ST_SetGeotransform(ST_MakeEmptyRaster(10,10, 0,0, 20), 
+                                  50, 85, radians(45), radians(90))) as gt) as foo ; 
