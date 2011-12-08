d47225798f1b0ec41a35b364f340ef8ade04380e
Bryce Nordgren <bnordgren@gmail.com>
2011-12-08 10:47:29 +0000

    got rid of geotransform type in favor of out parameters

--- raster/test/regress/rt_geotransform.sql
+++ raster/test/regress/rt_geotransform.sql
@@ -32,14 +32,5 @@ SELECT (gt).imag as imag,
                ST_SetGeotransform(ST_MakeEmptyRaster(10,10, 0,0, 20), 
                                   50, 85, radians(45), radians(90), 20, 30)) as gt) as foo ; 
 
-SELECT (gt).imag as imag, 
-       (gt).jmag as jmag, 
-       degrees((gt).theta_i) as theta_i, 
-       degrees((gt).theta_ij) as theta_ij,
-       (gt).xoffset as xoffset, 
-       (gt).yoffset as yoffset 
-  FROM (SELECT ST_GetGeotransform(
-               ST_SetGeotransform(ST_MakeEmptyRaster(10,10, 0,0, 20), 
-                                  (50, 85, radians(45), radians(90), 20, 30)::geotransform)) as gt) as foo ; 
                                   
                                   
